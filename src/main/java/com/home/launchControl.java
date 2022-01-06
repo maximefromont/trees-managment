f2ea35216b3e6620bf0b8d7e5ec9ee93c21c772f
2maxime0 <71760500+2maxime0@users.noreply.github.com>
2022-01-06 02:17:01 +0100

    Refactor + Modif pour BDD launchControl

--- src/main/java/com/home/launchControl.java
+++ src/main/java/com/home/launchControl.java
@@ -1,39 +1,84 @@
 package com.home;
 
 import com.association.Association;
+import com.association.AssociationDAO;
+import com.cotisation.Cotisation;
+import com.cotisation.CotisationDAO;
 import com.member.Member;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class launchControl {
 
-    public static void main() {
-        Member memberTest = new Member(4,"Jean","Sah","Quelque part","A noel bg");
-        Association assosTest = new Association();
-        System.out.println("Menu\n" +
-                "1: Payer sa cotisation\n" +
-                "2: RGPD\n" +
+    private static Member member;
+    private static Association associationMember;
+
+    public static void menu(Member memberRecu) {
+
+        //Initialisation des variable static member et association
+        member = memberRecu;
+        associationMember = AssociationDAO.getAssociationByMember(member);
+
+        //Affichage membre
+        System.out.println("\n" + member.toString());
+
+        //Affichage association
+        System.out.println("\n" + associationMember.toString());
+
+        System.out.println("\n" + "Menu : " + "\n" +
+                "1: Payer sa cotisation" + "\n" +
+                "2: RGPD" + "\n" +
                 "Quelle fonction voulez-vous effectuer ? : ");
-        int n;
-        Scanner S = new Scanner(System.in);
-        n=S.nextInt();
-        switch (n){
+        Scanner s = new Scanner(System.in);
+        switch (s.nextInt()){
             case 1:
-                int c=0;
-                while(c<=0) {
-                    System.out.println(memberTest.getCotisation() +
-                            "\nCombien voulez-vous payer ?");
-                    Scanner S1 = new Scanner(System.in);
-                    c = S1.nextInt();
-                }
-                memberTest.cotisation(c,assosTest);
-                System.out.println(memberTest.getCotisation()+"\n"+ memberTest.getAnneeCotisation());
+                payCotisation();
                 break;
-            case 2: memberTest.RGPD();
-            System.out.println("Fichier dans java project en format txt");
+            case 2:
+                displayRGPD();
                 break;
             case 3:
                 break;
+        }
+    }
+
+    private static void payCotisation() {
+
+        System.out.print("\n" + "Veuillez indiquer le montant de votre cotisation : ");
+        Scanner s = new Scanner(System.in);
+        CotisationDAO.createNewCotisation(member, s.nextInt(), true);
+
+        System.out.println("\n" + "Voici le résumé de votre cotisation : " + "\n");
+        ArrayList<Cotisation> cotisations = CotisationDAO.getAllCotisationForMember(member);
+        System.out.println(cotisations.get(cotisations.size() - 1).toString()); //Dernière cotisation pour ce membre
+
+        AssociationDAO.updateRecette(associationMember); //Update de la recette de l'association du membre
+        associationMember = AssociationDAO.getAssociationByMember(member); //Update de la variable static associationMember
+
+        System.out.println("\n" + associationMember.toString());
+    }
+
+    private static void displayRGPD() {
+        String fileName = member.getName()+"_"+member.getId()+"_data.txt";
+        String encoding = "UTF-8";
+        try{
+            PrintWriter writer = new PrintWriter(fileName, encoding);
+            writer.println(member.toString());
+            writer.println("--------------------");
+            for(Cotisation cotisation : CotisationDAO.getAllCotisationForMember(member)) {
+                writer.println(cotisation.toString());
+                writer.println("\n");
+            }
+            writer.close();
+
+            System.out.println("Un fichier de format txt à été imprimé dans java project.");
+        }
+        catch (IOException e){
+            System.out.println("Une erreur durant l'impression du fichier RGPD à eu lieu.");
+            e.printStackTrace();
         }
     }
 }
