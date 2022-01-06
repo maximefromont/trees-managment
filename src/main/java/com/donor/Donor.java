86123212a70eed2fd1453f581be97fcd2c387069
2maxime0 <71760500+2maxime0@users.noreply.github.com>
2022-01-06 02:17:58 +0100

    Informations Donor (a supprimer ?)

--- src/main/java/com/donor/Donor.java
+++ src/main/java/com/donor/Donor.java
@@ -5,6 +5,10 @@ import com.association.Association;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+//ATTENTION : Cette classe ne sert à rien et ne doit pas être utilisé, en effet, un donneur est un member avec un type "donor".
+//Les membres quand à eux sont de type "memberAssos". Il faudrait que je renomme la table "Member" en "User" dans la base de donnée mais pour l'instant j'ai la flemme.
+//J'ai gardé la classe pour le bout de code, je ne sais pas si tu en as besoin, si non, tu peux supprimer la classe et le package.
+
 public class Donor {
     private Association a;
     public Donor(Association assos){
